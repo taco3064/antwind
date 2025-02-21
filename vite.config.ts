c6e6505ae85dfc111c2774d8e570e9c1904621ba
taco3064 <tabacotaco@gmail.com>
2025-02-21 11:43:39 +0800

    fix(vite.config): fixed the error base settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
 
+const BASENAME = process.env.NODE_ENV !== 'production' ? '/' : '/antd-theme-architect';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: BASENAME,
   plugins: [react(), tsconfigPaths(), tailwindcss()],
   define: {
     'import.meta.env.VITE_DEMO_COMPONENTS': JSON.stringify(
       fs.readdirSync(path.resolve(__dirname, './src/checkouts')),
     ),
-    'import.meta.env.VITE_ROUTER_BASENAME': JSON.stringify(
-      process.env.NODE_ENV !== 'production' ? '/antd-theme-architect' : undefined,
-    ),
+    'import.meta.env.VITE_ROUTER_BASENAME': JSON.stringify(BASENAME),
   },
 });
