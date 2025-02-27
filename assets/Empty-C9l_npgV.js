cdbe9a6d1fedbb2584bd553e68d48cb9d36c7026
taco3064 <taco3064@users.noreply.github.com>
2025-02-27 07:57:42 +0000

    deploy: 40d1f99e112882dece69ab0441d20eca1418242e

---
+++ assets/Empty-C9l_npgV.js
@@ -0,0 +1,1 @@
+import{j as s,ae as t,D as e}from"./index-CIDXRfH1.js";const p=()=>s.jsx(t,{}),a=()=>s.jsx(t,{image:t.PRESENTED_IMAGE_SIMPLE});function i(){return s.jsx(e,{title:"Empty",items:{Basic:p,Simple:a}})}export{i as default};
